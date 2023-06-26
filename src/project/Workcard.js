780d3ab3d69c2f1f43a4d2c656c9a5762ee8bc9e
mahiiram <mahendran.ramar9797@gmail.com>
2023-06-26 15:31:24 +0530

    new commit

--- src/project/Workcard.js
+++ src/project/Workcard.js
@@ -14,27 +14,7 @@ const Workcard = () => {
             <div class="container">
                 <div class="row">
 
-                    <div class="col-lg-4">
-                        <div class="card mb-10 mb-lg-0">
-                            <div class="card-body">
-                                <h3 class="card-title text-uppercase text-center" >ADD TO CART</h3>
-                                <img src={addcart} alt="cart" />
-                                <div>
-                                    <p>
-                                        The add-to-cart button is a feature of ecommerce
-                                        stores that allows customers to choose items to purchase
-                                        without actually completing the payment.
-                                        For online stores, it lives on individual product pages,
-                                        functioning as the digital equivalent of a shopping cart in a brick and mortar store.
-                                    </p>
-                                </div>
-                                <div class="d-grid btn-section">
-                                    <NavLink to="https://fantastic-ganache-164d55.netlify.app/" className='btn' >View</NavLink>
-                                    <NavLink to="https://github.com/mahiiram/shoppingCart" className='btn' >Source</NavLink>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            
                     <div class="col-lg-4">
                         <div class="card mb-10 mb-lg-0">
                             <div class="card-body">
@@ -51,62 +31,6 @@ const Workcard = () => {
                                 <div class="d-grid btn-section">
                                     <NavLink to="https://jolly-choux-427a1a.netlify.app/" className='btn' >View</NavLink>
                                     <NavLink to="https://github.com/mahiiram/dictinaryapi" className='btn' >Source</NavLink>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4">
-                        <div class="card mb-10 mb-lg-0">
-                            <div class="card-body">
-                                <h3 class="card-title text-uppercase text-center">CALCULATOR</h3>
-                                <img src={calc} alt="calc" />
-                                <div>
-                                    <p>
-                                    A machine for performing arithmetical operations 
-                                    (as multiplication) that are usually more complex than 
-                                    can be done on an adding machine
-                                    </p>
-                                </div>
-                                <div class="d-grid btn-section">
-                                    <NavLink to="https://gleeful-clafoutis-2ce1ac.netlify.app/" className='btn' >View</NavLink>
-                                    <NavLink to="https://github.com/mahiiram/day15calculator" className='btn' >Source</NavLink>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4">
-                        <div class="card mb-10 mb-lg-0">
-                            <div class="card-body">
-                                <h3 class="card-title text-uppercase text-center">urlshortener</h3>
-                                <img src={url} alt="calc" />
-                                <div>
-                                    <p>
-                                    URL sortner its shorts the long url into short,if you are a registered user you can be use this shortner Otherwise not.After login, the url shorner form Will come. You can using it after login 
-                                    </p>
-                                </div>
-                                <div class="d-grid btn-section">
-                                    <NavLink to="https://tranquil-haupia-9236a4.netlify.app/" className='btn' >View</NavLink>
-                                    <NavLink to="https://github.com/mahiiram/urlshortener-frontend" className='btn' >Source</NavLink>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4">
-                        <div class="card mb-10 mb-lg-0">
-                            <div class="card-body">
-                                <h3 class="card-title text-uppercase text-center">Movie BOOKING</h3>
-                                <img src={moviebooking} alt="calc" />
-                                <div>
-                                    <p>
-                                    This is a simple movie booking app created with both Reactjs and Nodejs.you can add and delete a movie as an Admin. if You are user you can booking the movies only.  
-                                    </p>
-                                </div>
-                                <div class="d-grid btn-section">
-                                    <NavLink to="https://tubular-valkyrie-8e79c8.netlify.app/" className='btn' >View</NavLink>
-                                    <NavLink to="https://github.com/mahiiram/wecode-frontend-ticketbooking" className='btn' >Source</NavLink>
                                 </div>
                             </div>
                         </div>
